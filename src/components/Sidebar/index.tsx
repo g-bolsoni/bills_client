a1267571bc5bb24cbd5028e21b06242abbd4d682
g-bolsoni <giovanebolsoni2@gmail.com>
2023-12-10 22:54:42 -0300

    Change Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,14 +4,17 @@ import Link from 'next/link'
 
 export const Sidebar: React.FC = () => {
   return (
-    <section className="min-h-screen h-full w-72 bg-gray-800">
-        <ul className="flex flex-col">
-            <li>
+    <section className="min-h-screen h-full w-72 bg-gray-800 flex flex-col  items-center">
+        <span className="text-lg font-bold italic text-slate-200 h-16 w-full flex items-center justify-center cursor-pointer">
+          GBFinance
+        </span>
+        <ul className="flex flex-col items-start mt-3 w-full gap-2 px-5">
+            <li className=" text-slate-200 text-base font-medium hover:text-slate-400">
               <Link href="/">
                 Minhas contas
               </Link>
             </li>
-            <li>
+            <li  className="text-slate-200 text-base font-medium hover:text-slate-400">
               <Link href="/addBills">
                 Adicionar Conta
               </Link>
