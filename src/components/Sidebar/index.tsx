6bb92f985517499dc585b50f3b8bc43df1404143
g-bolsoni <giovanebolsoni2@gmail.com>
2023-12-11 20:57:38 -0300

    Create modal component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Link from 'next/link'
-
+import { Button } from 'flowbite-react';
+import { useState } from 'react';
 
 export const Sidebar: React.FC = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [email, setEmail] = useState('');
+
+
   return (
-    <section className="min-h-screen h-full w-72 bg-gray-800 flex flex-col  items-center">
+    <section className="min-h-screen h-full w-72 bg-gray-800 flex flex-col relative">
+      <div>
         <span className="text-lg font-bold italic text-slate-200 h-16 w-full flex items-center justify-center cursor-pointer">
           GBFinance
         </span>
@@ -13,12 +20,11 @@ export const Sidebar: React.FC = () => {
                 Minhas contas
               </Link>
             </li>
-            <li  className="text-slate-200 text-base font-medium hover:text-slate-400">
-              <Link href="/addBills">
-                Adicionar Conta
-              </Link>
-            </li>
         </ul>
+      </div>
+      <div className="absolute bottom-5 left-5 max-w-[288px]" >
+        <Button onClick={() => setOpenModal(true)} className=''>Adicionar Conta</Button>
+      </div>
     </section>
 
   );
