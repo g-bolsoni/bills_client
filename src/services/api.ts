0b6d9ab2f6c56ca38f01ae88e936acbc83efb30f
Giovane Souza <giovane.souza@irroba.com.br>
2024-02-07 14:58:53 -0300

    Change base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 
+
+let baseUrl = 'https://bills-api-oiyw.onrender.com';
+
+if (process.env.NODE_ENV === "development") {
+    baseUrl = 'http://localhost:3333';
+}
+
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: baseUrl
 });
 
 export default api;
